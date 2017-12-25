5aa37a3c1574db5a41e781a44331ed75d6617023
Travis Chan <travismchan@gmail.com>
2017-12-25 14:57:16 +1100

    Play around with hasMany and belongsTo

--- app/components/blog-post.js
+++ app/components/blog-post.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
+const { computed } = Ember;
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
   isEditing: false,
-
+  showCommentTextArea: false,
+  comment: null,
+  hasComments: computed.readOnly('post.comments.length'),
+  hasCommentsText: computed('hasComments', function () {
+    if (this.get('hasComments') === 1) {
+      return 'comment';
+    }
+    return 'comments';
+  }),
   actions: {
     deletePost(post) {
       post.destroyRecord();
@@ -18,6 +28,21 @@ export default Ember.Component.extend({
       post.save().then(() => {
         this.set('isEditing', false);
       });
+    },
+
+    showCommentTextArea() {
+      this.set('showCommentTextArea', true);
+    },
+    saveComment(comment, post) {
+      let blogComment = this.get('store').createRecord('comment', {message: comment});
+      // Firebase needs to save top layer first for hasMany relationships after adding an object
+      // and then saving the individual blog record
+      post.get('comments').then((comments) => {
+        comments.addObject(blogComment);
+        post.save().then(() => {
+          blogComment.save();
+        })
+      })
     }
   }
 
---
+++ app/models/comment.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  message: DS.attr('string'),
+  post: DS.belongsTo()
+});
